0e17868fae0cfbc95ecddf9d280814b490602a8b
Martina <48300488+martinakbrehm@users.noreply.github.com>
2023-08-25 17:07:29 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+import pygame
+
+from jogodaonca.constantes import LARGURA, ALTURA, TAMANHO_POSICAO
+from jogodaonca.jogo import Jogo
+
+def main():
+
+    jogo = Jogo()
+
+    jogo.interface.click()
+
+main()
+
